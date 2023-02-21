ba57201b7ee525b0faee5259fa06e7b0d9aae347
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-21 17:13:44 +0100

    feat: add search bar

--- src/presentation/Screens/Events/index.tsx
+++ src/presentation/Screens/Events/index.tsx
@@ -1,19 +1,69 @@
-import { Box, HStack, Text } from "native-base"
-import { useNavigation } from '@react-navigation/native'
+import { useCallback, useState } from 'react'
+import { Box, HStack, Text, VStack } from "native-base"
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
 
-import { useEvent } from "../../../hooks/useEvent"
 import { Event } from "../../components/Event"
 import { Button } from "../../components/Button"
 import { Layout, LayoutBody, LayoutHeader } from "../../components/Layout"
 import { TextTitle } from "../../components/TextTitle"
+import { IEvent } from '../../../@types/event'
+import { makeAxiosHttpClient } from '../../../main/factory/axios-http-client-factory'
+import { Loading } from '../../components/Loading'
+import { HttpStatusCode } from '../../../data/protocol/http'
+import { Alert } from 'react-native'
+import { useAuth } from '../../../hooks/useAuth'
+import { SearchBar } from '../../components/SearchBar'
 
 export function Events () {
-  const { events } = useEvent()
   const { navigate } = useNavigation()
+  const { signOut } = useAuth()
+
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  const [events, setEvents] = useState<IEvent[]>([])
+  const [filteredEvents, setFilteredEvents] = useState<IEvent[]>([])
+
+  useFocusEffect(useCallback(() => {
+    fetchEvents()
+  }, []))
+
+  async function fetchEvents () {
+    setIsLoading(true)
+    const { statusCode, body } = await makeAxiosHttpClient().get({ url: '/events' })
+
+    if (statusCode === HttpStatusCode.unauthorized) {
+      Alert.alert('Sessão expirada', 'A sua sessão expirou, inicie novamente para continuar.')
+      signOut()
+    } else {
+      setEvents(body.events)
+      setFilteredEvents(body.events)
+    }
+    setIsLoading(false)
+  }
+
+  const filterEvents = (text: string) => {
+    const typedText = text.trim()
+    console.log('typedText', typedText)
+
+    if (typedText) {
+      const filterdEvents = events.filter(event => {
+        return event.title.toLowerCase().includes(typedText.toLowerCase())
+      })
+      console.log('filterdEvents', filterdEvents.map(e => e.title))
+
+      setFilteredEvents(filterdEvents)
+      return
+    }
+
+    setFilteredEvents(events)
+  }
+
+  if (isLoading) return <Loading />
 
   return (
     <Layout>
       <LayoutHeader>
+        <SearchBar initialValue='Olá' onChangeText={filterEvents} />
         <TextTitle title="Eventos" />
       </LayoutHeader>
 
@@ -38,26 +88,19 @@ export function Events () {
             type="PRIMARY"
             onPress={() => navigate('newevent')}
           />
-
-          <HStack
-            w={'full'}
-            mt={3}
-            bg={'white'}
-            space={4}
-            direction={'column'}
-          >
-            <Box mt={3} >
-              <Text
-                fontSize={20}
-              >
-                Lista de eventos ({events.length})
-              </Text>
-            </Box>
-            {events.map(event => (
-              <Event key={event.id} event={event} />
-            ))}
-          </HStack>
         </Box>
+
+        <VStack mt={5} space={5}>
+          {filteredEvents.length < 1 ?
+            <Box alignItems={'center'}>
+              <Text>Nenhum evento de momento.</Text>
+            </Box>
+            :
+            events.map((event) => (
+              <Event key={event.id} event={event} />
+            ))
+          }
+        </VStack>
       </LayoutBody>
     </Layout>
   )
