8da23f6fb3f2cacde38b84584a7414066c77def7
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-22 22:05:42 +0100

    feat: user list for test

---
+++ src/utils/data/user.ts
@@ -0,0 +1,37 @@
+import { IUser } from '../../@types/user'
+import { DateUtils } from '../date-utils'
+
+export const UserListData = [
+  {
+    id: '1',
+    name: 'Admin',
+    email: 'admin@gmail.com',
+    username: 'admin',
+    password: 'admin',
+    createdAt: DateUtils.randonDate().toDateString(),
+  },
+  {
+    id: '2',
+    name: 'Samuel Freitas',
+    email: 'samuelfreitas.ao@gmail.com',
+    username: 'samuel',
+    password: '123456',
+    createdAt: DateUtils.randonDate().toDateString(),
+  },
+  {
+    id: '3',
+    name: 'Jeorgel Dev',
+    email: 'jeorgel@gmail.com',
+    username: 'jeorgel',
+    password: '123456',
+    createdAt: DateUtils.randonDate().toDateString(),
+  },
+  {
+    id: '4',
+    name: 'Solange',
+    email: 'solange@gmail.com',
+    username: 'solange',
+    password: '123456',
+    createdAt: DateUtils.randonDate().toDateString(),
+  },
+] as IUser[]
