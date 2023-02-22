116cb79755a66b3fee6186278fdef6995b9d3226
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-22 19:05:01 +0100

    feat: new logo with text

--- src/presentation/components/Logo.tsx
+++ src/presentation/components/Logo.tsx
@@ -1,11 +1,12 @@
-import { Image, Text, IImageProps } from "native-base"
+import { Image, IImageProps } from "native-base"
 
 const LogoImg = require('../../../assets/logo.png')
 const LogoImgLg = require('../../../assets/logotipo.png')
+const LogoTex = require('../../../assets/logo-text.png')
 
 export function Logo () {
   return (
-    <Image source={LogoImg} width={20} height={20} alt='Logotipo RCS' />
+    <Image source={LogoImg} width={20} height={20} alt='Logotipo Trybu' />
   )
 }
 
@@ -13,7 +14,17 @@ export function LogoLG (props: IImageProps) {
   return (
     <Image
       source={LogoImgLg}
-      alt='Logotipo RCS'
+      alt='Logotipo Trybu'
+      {...props}
+    />
+  )
+}
+
+export function LogoText (props: IImageProps) {
+  return (
+    <Image
+      source={LogoTex}
+      alt='Logotipo Trybu'
       {...props}
     />
   )
